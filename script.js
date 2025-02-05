45697f50eea8fad9bd637385bee984ea92fd1c1a
gustavopenha07 <gustavopenha085@gmail.com>
2025-02-05 07:42:25 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //Substituir a imagem
+  if(html.classList.contains('light')) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute('src', './assents/avatar-light.png')
+  } else {
+    //se tiver sem light mode, manter a imagem normal
+    img.setAttribute('src', './assents/avatar.png')
+
+  }
+
+ 
+}
